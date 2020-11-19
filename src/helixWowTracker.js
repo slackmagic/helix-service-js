fa2033671264e83cf94090ceaa34fcf6f44849ae
SlackMagiC <laurent.pietrzyk@gmail.com>
2020-11-19 18:24:09 +0100

    ENH: Update wow tracker

--- src/helixWowTracker.js
+++ src/helixWowTracker.js
@@ -1,14 +1,23 @@
 import HelixService from "./helixService";
 
+
 const CHARACTERS = "/api/wow-tracker/characters";
-const REFRESH = "/api/wow-tracker/refresh";
+const CHARACTERS_LAST_DATA = "/api/wow-tracker/characters/last";
+const CHARACTERS_PREVIOUS_DATA = "/api/wow-tracker/characters/previous";
+const UPDATE_CHARACTERS = "/api/wow-tracker/characters/update";
 
 const HelixWowTracker = {
-	getCharacters(callback) {
+	getCharactersAllData(callback) {
 		return HelixService.getWithAuth(CHARACTERS, null);
 	},
-	refresh(callback) {
-		return HelixService.getWithAuth(REFRESH, null);
+	getCharactersLastData(callback) {
+		return HelixService.getWithAuth(CHARACTERS_LAST_DATA, null);
+	},
+	getCharactersPrevioudData(callback) {
+		return HelixService.getWithAuth(CHARACTERS_PREVIOUS_DATA, null);
+	},
+	updateCharacters(callback) {
+		return HelixService.getWithAuth(UPDATE_CHARACTERS, null);
 	},
 };
 
